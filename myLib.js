5ebccfd2ff25f9b79dba0573dbd289c311561af1
TulikaTamuly <73019655+TulikaTamuly@users.noreply.github.com>
2020-10-30 13:30:35 +0530

    Update myLib.js

--- myLib.js
+++ myLib.js
@@ -1,8 +1,8 @@
 function isTouching(car,wall){
-    if (car.x - wall.x <car.width/2 + wall.width/2
-      && wall.x - car.x < wall.width/2 + car.width/2
-      && car.y - wall.y < wall.height/2 + car.height/2
-      && wall.y - car.y < wall.height/2 + car.height/2) {
+    if (bullet.x - wall.x <bullet.width/2 + wall.width/2
+      && wall.x - car.x < wall.width/2 + bullet.width/2
+      && bullet.y - wall.y < wall.height/2 + bullet.height/2
+      && wall.y - car.y < wall.height/2 + bullet.height/2) {
    return true
   }
   else {
